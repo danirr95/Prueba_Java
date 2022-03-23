17e066761519e3ffa3a8b62b9ba5361ff4e8958b
DANI <DANI@DANI-PC>
2022-03-23 11:42:08 +0100

    Segunda versión

--- Prueba_Java/src/Prueba_GitHub.java
+++ Prueba_Java/src/Prueba_GitHub.java
@@ -2,6 +2,9 @@ public class Prueba_GitHub {
 	
 	public static void main (String args[]) {
 		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+		
+		System.out.println("Frase para la segunfa versión");
+
 	}
 
 }
